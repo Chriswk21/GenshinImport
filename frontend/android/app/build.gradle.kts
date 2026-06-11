3dcdc6db8f6cbb3c655346e405140860c3cda830
Chriswk21 <chriswk2103@gmail.com>
2026-06-11 09:49:53 +0700

    Share debug keystore to unify SHA-1 signature

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -26,6 +26,15 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
